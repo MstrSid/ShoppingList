02a352c77b839160128e212e44e5967ac97e4242
Eugene Kos <mstrsid@live.com>
2021-07-22 19:51:05 +0300

    done add data layer

---
+++ app/src/main/java/by/kos/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,41 @@
+package by.kos.shoppinglist.data
+
+import by.kos.shoppinglist.domain.ShopItem
+import by.kos.shoppinglist.domain.repositories.ShopListRepository
+import java.lang.RuntimeException
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if(shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId
+            autoIncrementId++
+        }
+            shopList.add(shopItem)
+
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shoItem: ShopItem) {
+        val old = getShopItem(shoItem.id)
+        shopList.remove(old)
+        addShopItem(shoItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Element with id $shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+}
