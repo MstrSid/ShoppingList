c53298b758cfd56c683e78c7cb37e2b41dbd4c54
Eugene Kos <mstrsid@live.com>
2021-07-22 19:21:55 +0300

    done add data layer chapter 1

---
+++ app/src/main/java/by/kos/shoppinglist/domain/usecases/GetShopItemUseCase.kt
@@ -0,0 +1,9 @@
+package by.kos.shoppinglist.domain.usecases
+
+import by.kos.shoppinglist.domain.ShopItem
+
+class GetShopItemUseCase {
+    fun getShopItem(shopItemId: Int): ShopItem{
+        TODO()
+    }
+}
---
+++ app/src/main/java/by/kos/shoppinglist/domain/usecases/GetShopListUseCase.kt
@@ -0,0 +1,9 @@
+package by.kos.shoppinglist.domain.usecases
+
+import by.kos.shoppinglist.domain.ShopItem
+
+class GetShopListUseCase {
+    fun getShopList(): List<ShopItem>{
+        TODO()
+    }
+}
